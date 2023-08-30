a2aa4277026b31072caf9e5debbe45b66c6bdf4b
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-08-29 23:38:17 -0400

    Update studentaddress.java

--- studentaddress.java
+++ studentaddress.java
@@ -1,32 +1,31 @@
-package selfwork;
+package StudentSelfProject;
 
-public class studentaddress {
-	
-	private String address;
-	private String zip_code;
-	private String state;
-	private String city;
-	
-	public studentaddress(String address, String zip_code, String state, String city) {
-		this.address = address;
-		this.zip_code = zip_code;
-		this.state = state;
-		this.city = city;
-	}
-		public String getAddress() {
-			return address;
-		}
-		public String getZipcode() {
-			return zip_code;
-		}
-		public String getState() {
-			return state;
-		}
-		public String getCity() {
-			return city;
-		}
-		
-		
-	}
+public class StudentAddress {
 
+    private String address;
+    private String zip_code;
+    private String state;
+    private String city;
+
+    public StudentAddress(String address, String zip_code, String state, String city) {
+        this.address = address;
+        this.zip_code = zip_code;
+        this.state = state;
+        this.city = city;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getZipcode() {
+        return zip_code;
+    }
+    public String getState() {
+        return state;
+    }
+    public String getCity() {
+        return city;
+    }
+
+
+}
 
