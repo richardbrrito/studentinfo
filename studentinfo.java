1baf7bb0afb2c5d432a0cc7ae9ce99ad28781d6f
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-08-29 23:39:23 -0400

    Update studentinfo.java

--- studentinfo.java
+++ studentinfo.java
@@ -1,35 +1,52 @@
-package selfwork;
+package StudentSelfProject;
 
-public class studentinfo {
-	
-	private String FirstName;
-	private String LastName;
-	private String School;
-	private double Grade;
-	private double gpa;
-	
-	public studentinfo(String FirstName, String LastName, String School, double Grade, double gpa) {
-		this.FirstName = FirstName;
-		this.LastName = LastName;
-		this.School = School;
-		this.Grade = Grade;
-		this.gpa = gpa;
-	}
-	
-	//getters
-	public String FirstName() {
-		return FirstName;
-	}
-	public String LastName() {
-		return LastName;
-	}
-	public String School() {
-		return School;
-	}
-	public double Grade() {
-		return Grade;
-	}
-	public double gpa() {
-		return gpa;
-	}
+public class StudentInfo {
+
+    private String FirstName;
+    private String LastName;
+    private String School;
+    private double Grade;
+    private double Gpa;
+
+    public StudentInfo(String FirstName, String LastName, String School, int Grade, double Gpa) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.School = School;
+        this.Grade = Grade;
+        this.Gpa = Gpa;
+    }
+
+    //getters
+    public String getFirstName() {
+        return FirstName;
+    }
+    public String getLastName() {
+        return LastName;
+    }
+    public String getSchool() {
+        return School;
+    }
+    public double getGrade() {
+        return Grade;
+    }
+    public double getGpa() {
+        return Gpa;
+    }
+    //setters
+    public void setFirstName(String FirstName){
+        this.FirstName = FirstName;
+    }
+    public void setLastName(String LastName){
+        this.LastName = LastName;
+    }
+    public void setSchool(String School){
+        this.School = School;
+    }
+
+    public void setGpa(double Gpa) {
+        this.Gpa = Gpa;
+    }
+    public void setGrade(int Grade){
+        this.Grade = Grade;
+    }
 }
