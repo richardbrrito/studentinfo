9e5d1cfd08784f0ddaa4321370aecd21efb610a4
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-08-30 00:51:27 -0400

    Update Database.java
    fixed if statement in for loop

--- Database.java
+++ Database.java
@@ -3,16 +3,16 @@ import java.util.ArrayList; //arraylist import
 public class Database {
 
     private static Database instance;
-//holds the single instance of the database class
-    private ArrayList<StudentInfo> StudentList; //Makes an arraylist to to store the Students
-    private StudentInfo currentStudent; // Holds name of current student
+    //holds the single instance of the database class
+    private ArrayList<StudentInfo> StudentList; //Makes an arraylist to store the Students
+    private StudentInfo CurrentStudent; // Holds name of current student
     private int CurrentIndex; // Keeps track of the index of the arraylist
     private boolean found; // Checks to see if the student has been found
 
     private Database() { // Constructor
         this.StudentList = new ArrayList<>();
-        this.currentStudent = null;
-        this.currentIndex = -1;
+        this.CurrentStudent = null;
+        this.CurrentIndex = -1;
         this.found = false;
     }
     public static Database getInstance() { // If the instance doesn't exist, it makes one.
@@ -25,19 +25,19 @@ public class Database {
     // Search Student By Name
     public void search(String StudentName){ // method to perform search
         found = false; // false so that if found the if statement makes it true
-        for (int i= 0; i < student.size(); i++){ // for statement that runs throughout the whole student list
-            if(student.size(i).getFirstName().equals(StudentName)){
-                currentStudent = student.get(i); // set currentStudent to the matching student
-                currentIndex = i; // set index in array to the one of the student
+        for (int i= 0; i < StudentList.size(); i++){ // for statement that runs throughout the whole student list
+            if(StudentList.get(i).getFirstName().equals(StudentName)){ // if studentlist size = first name run if statement
+                CurrentStudent = StudentList.get(i); // set currentStudent to the matching student
+                CurrentIndex = i; // set index in array to the one of the student
                 found = true; // true if student name is found
                 break; // break because if found it doesnt need to keep searching
             }
-            public void add(StudentInfo newStudent){
-                StudentList.add(newStudent); // adds newStudent to arraylist StudentList
             }
         }
+    public void add(StudentInfo newStudent){
+        StudentList.add(newStudent); // adds newStudent to arraylist StudentList
 
-}
+    }
 
 
 
